3b555eb91135dd1929498f9ee2a2d1c7d6569c5e
Sai Hein Thet Aung <101163596+SaiHeinThetAung@users.noreply.github.com>
2023-10-22 20:16:19 +0630

    Update EachDetail.jsx

--- client/src/EachDetail.jsx
+++ client/src/EachDetail.jsx
@@ -15,8 +15,8 @@ const EachDetail = () => {
               <h2 className='text-2xl text-yellow-400 font-bold'>{state.ProverbName}</h2>
           </div>
       
-        <div className='mx-auto w-[300px] shadow-lg p-4 mb-4'>
-          <img className='w-[600px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO6gl8D3OzkpqmqJW5SlWLRbP2VhSEAZXUMg&usqp=CAU" alt="" />
+        <div className='mx-auto w-[400px] shadow-lg p-10 mb-4'>
+          <img className='w-[400px] mb-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO6gl8D3OzkpqmqJW5SlWLRbP2VhSEAZXUMg&usqp=CAU" alt="" />
             <h3 className='mb-4 text-center font-semibold text-lg'>အဓိပ္ပာယ်ဖွင့်ဆိုချက်</h3>
             <p>{state.ProverbDesp}</p>
         </div>
