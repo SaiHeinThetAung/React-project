f8aa50576d53002a09aa049b8de0d150d5413666
Sai Hein Thet Aung <101163596+SaiHeinThetAung@users.noreply.github.com>
2023-10-22 20:03:59 +0630

    Update EachDetail.jsx

--- client/src/EachDetail.jsx
+++ client/src/EachDetail.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import {  useLocation, useNavigate } from 'react-router-dom'
 import {BsFillJournalBookmarkFill} from 'react-icons/bs'
 
 const EachDetail = () => {
     const {state}=useLocation()
+    const nav=useNavigate()
     console.log(state)
   return (
     <div>
       <div className='flex flex-col mt-10 gap-4'>
-       <div className='flex gap-3'>
-          <BsFillJournalBookmarkFill className='text-3xl text-yellow-400'/>
-          <h2 className='text-2xl text-yellow-400 font-bold'>{state.ProverbName}</h2>
-       </div>
-        <div className='mx-auto'>
-            <h3 className='mb-4 text-center font-semibold text-lg'>အဓိပ္ပယ်ဖွင့်ဆိုချက်</h3>
+       
+          <div onClick={()=>nav(-1)} className='flex gap-3 cursor-pointer hover:focus:peer-focus-visible'>
+              <BsFillJournalBookmarkFill className='text-3xl text-yellow-400'/>
+              <h2 className='text-2xl text-yellow-400 font-bold'>{state.ProverbName}</h2>
+          </div>
+      
+        <div className='mx-auto w-[300px] shadow-lg p-4 mb-4'>
+          <img className='w-[600px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO6gl8D3OzkpqmqJW5SlWLRbP2VhSEAZXUMg&usqp=CAU" alt="" />
+            <h3 className='mb-4 text-center font-semibold text-lg'>အဓိပ္ပာယ်ဖွင့်ဆိုချက်</h3>
             <p>{state.ProverbDesp}</p>
         </div>
       </div>
